fd764565ad7cfb959c0c2d18573b7d9f230da4a7
bbod <bradybod@gmail.com>
2019-12-10 20:18:19 -0700

    tested iterator

--- main.cpp
+++ main.cpp
@@ -7,5 +7,19 @@ int main() {
   array.push_back(3);
   array.push_back(4);
   std::cout << array[2] << std::endl;
+
+  dynamic_array array2 = dynamic_array<int, 6>{2,3,4};
+
+  bool check = array==array2;
+  std::cout << "check if arrays are equal " << check << std::endl;
+
+  std::cout << "itterate over array" << std::endl;
+
+  auto f = std::find(array.begin(), array.end(), 2);
+  if (f != array.end())
+    std::cout << "Element found in array: " << *f << '\n';
+  else
+    std::cout << "Element not found in array\n";
+
   return 0;
 }
